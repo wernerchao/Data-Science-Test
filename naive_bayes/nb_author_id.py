4a2689731db189de71ef1ea5491b3c854fb0b92b
Werner Chao <oahcrenrew@gmail.com>
2016-10-15 23:10:56 +0800

    added SVM model

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,15 +15,28 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
-clf = GaussianNB()
+# clf = GaussianNB()
+clf = SVC(kernel='rbf', C=10000.0)
+
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
 pred = clf.predict(features_test)
+print "testing time:", round(time()-t1, 3), "s"
+
+t2 = time()
 score = clf.score(features_test, labels_test)
+print "accuracy time:", round(time()-t2, 3), "s"
 
 print score
