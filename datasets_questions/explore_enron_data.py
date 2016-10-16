c497264fb46674d9f9edc41267535838ae185bf1
Werner Chao <oahcrenrew@gmail.com>
2016-10-16 23:33:54 +0800

    added KNN and started ENRON project

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,11 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print "# of data points(persons) is: ", len(enron_data)
+print "# of features is ", len(enron_data["SKILLING JEFFREY K"])
 
+poiNum = 0
+for person_name in enron_data:
+    if enron_data[person_name]["poi"]:
+        poiNum += 1
+print "# of POI is: ", poiNum
